9b61f08cd521e6bc267f6fb65e545334f1859222
赵宏健 <zhaohongjian@192.168.31.176>
2026-03-22 20:33:22 +0800

    update

--- audio-text/dev.sh
+++ audio-text/dev.sh
@@ -7,7 +7,7 @@ pip install pip-tools -i https://mirrors.aliyun.com/pypi/simple
 pip-compile requirements.txt --output-file requirements-locked.txt 
 #已存在locked文件时使用
 pip install -r requirements-locked.txt -i https://mirrors.aliyun.com/pypi/simple
-MODEL_PATH=$(pwd)/models NO_PROXY=192.168.246.22 HF_ENDPOINT=https://hf-mirror.com uvicorn app:app --host 0.0.0.0 --port 8000 --reload
+MODEL_PATH=$(pwd)/models NO_PROXY=192.168.246.22 HF_ENDPOINT=https://hf-mirror.com uvicorn app:app --host 0.0.0.0 --port 15001 --reload
 
 
 # 下载模型 
