b6042a582760468ba3bc77b7193db112f7e280ac
赵宏健 <zhaohongjian@192.168.31.176>
2026-03-22 20:33:11 +0800

    update

--- ocr/dev.sh
+++ ocr/dev.sh
@@ -7,4 +7,4 @@ pip install pip-tools -i https://mirrors.aliyun.com/pypi/simple
 pip-compile requirements.txt --output-file requirements-locked.txt  
 #已存在locked文件时使用
 pip install -r requirements-locked.txt -i https://mirrors.aliyun.com/pypi/simple
-NO_PROXY=192.168.246.22 HF_ENDPOINT=https://hf-mirror.com uvicorn app:app --host 0.0.0.0 --port 8000 --reload
+NO_PROXY=192.168.246.22 HF_ENDPOINT=https://hf-mirror.com uvicorn app:app --host 0.0.0.0 --port 15004 --reload
